4d8fb2f784887df883bcc7929a5844a79757adb2
BobbyWasabiReal <wasabibob13@gmail.com>
2022-12-09 17:16:13 -0800

    Thank you Jim and Devlin

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -5,7 +5,7 @@ export default async function sendRequest(url, method = 'GET', payload = null) {
     // used to include a data payload, set headers, etc. 
     const options = { method };
     if (payload) {
-        options.heders = { 'Content-Type': 'application/json' };
+        options.headers = { 'Content-Type': 'application/json' };
         options.body = JSON.stringify(payload);
     }
     const token = getToken();
