ab1634dd4f0ddbf7d95832ebf8e80d7e31ce9303
BobbyWasabiReal <wasabibob13@gmail.com>
2022-12-14 09:32:55 -0800

    Initial commit

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LogInForm from "../../components/LogInForm/LogInForm";
 
 export default function AuthPage({ setUser }) {
+    const [showSignUpForm, setShowSignUpForm] = useState(true);
+
     return (
         <main>
-            <h1>AuthPage</h1>
-            <SignUpForm setUser={setUser} />
-            <LogInForm setUser={setUser} />
+            <button onClick={() => setShowSignUpForm(!showSignUpForm)}>{showSignUpForm ? 'Sign Up' : 'Log In' }</button>
+            {showSignUpForm ? <SignUpForm setUser={setUser} /> : <LogInForm setUser={setUser} />}
         </main>
     );
 }
