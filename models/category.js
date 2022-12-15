ca25d2394688b73774c1213727a764d594c881a3
BobbyWasabiReal <wasabibob13@gmail.com>
2022-12-14 16:46:35 -0800

    small changes to model

--- models/category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const categorySchema = new Schema({
-    name: { type: String, required: true },
-    sortOrder: Number,
-});
-
-module.exports = mongoose.model('Category', categorySchema);
--- models/item.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-require('./categorySchema');
-const itemSchema = require('./itemSchema');
-
-module.exports = mongoose.model('Item', itemSchema);
