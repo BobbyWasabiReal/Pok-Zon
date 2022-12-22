c85cbd801f905dc0f2078977bcb184e80e22a6c5
BobbyWasabiReal <wasabibob13@gmail.com>
2022-12-22 01:29:10 -0800

    small changes

--- src/components/OrdersList/OrdersList.jsx
+++ src/components/OrdersList/OrdersList.jsx
@@ -2,14 +2,24 @@ import OrderListItem from "../OrderListItem/OrderListItem";
 import "./OrderList.css";
 
 export default function OrdersList({ orders, activeOrder, setActiveOrder }) {
-  const orderList = orders.map(order => 
+  const orderList = orders.map((order) => (
     <OrderListItem
       order={order}
       activeOrder={activeOrder}
       setActiveOrder={setActiveOrder}
       key={order.id}
     />
-    );
+  ));
 
-  return <div className={`OrderList ${orders.length ? '' : 'no-orders'}`}>{orderList}</div>;
+  return (
+    <div className={`OrderList ${orders.length ? "" : "no-orders"}`}>
+      {orders.length ? (
+        <>{orderList}</>
+      ) : (
+        <h1 style={{ position: "absolute", right: "40%", top: "14%" }}>
+          No Orders Yet... :(
+        </h1>
+      )}
+    </div>
+  );
 }
